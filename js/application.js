32d6ef005db6b86fd969070922af77076d9f5967
Javier Abadia <javier.abadia@esri.es>
2013-05-27 16:51:56 +0200

    prueba de captura de video en HTML5

--- js/application.js
+++ js/application.js
@@ -1,68 +1,45 @@
-// Some general UI pack related JS
-// Extend JS String with repeat method
-String.prototype.repeat = function(num) {
-    return new Array(num + 1).join(this);
-};
-
 (function($) {
+  "use strict";
 
-  // Add segments to a slider
-  $.fn.addSliderSegments = function (amount) {
-    return this.each(function () {
-      var segmentGap = 100 / (amount - 1) + "%"
-        , segment = "<div class='ui-slider-segment' style='margin-left: " + segmentGap + ";'></div>";
-      $(this).prepend(segment.repeat(amount - 2));
+  var canvas = $('#canvas').get(0);
+  console.log(canvas);
+  var context = canvas.getContext("2d");
+  var video = $('#video').get(0);
+  var videoProperties = { "video": true };
+  var errBack = function(error) {
+    console.log("Video capture error: ", error.code);
+  }
+
+  if(navigator.getUserMedia)
+  {
+    navigator.getUserMedia(videoProperties, function(stream)
+    {
+      video.src = stream;
+      video.play();
+    }, errBack);
+  } 
+  else if (navigator.webkitGetUserMedia)
+  {
+    navigator.webkitGetUserMedia(videoProperties, function(stream)
+    {
+      video.src = window.webkitURL.createObjectURL(stream);
+      video.play();
+    }, errBack);
+  }
+
+  $("#snap").click( function()
+  {
+    context.drawImage(video,0,0,320,240);
+    var data = canvas.toDataURL();
+    $.ajax({
+      type: "POST",
+      url: "upload.php",
+      data: { imgBase64: data }
+    }).done(function(o)
+    {
+      console.log('saved');
+      console.log(o);
     });
-  };
-
-  $(function() {
-  
-    // Todo list
-    $(".todo li").click(function() {
-        $(this).toggleClass("todo-done");
-    });
-
-    // Custom Select
-    $("select[name='herolist']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
-
-    // Tooltips
-    $("[data-toggle=tooltip]").tooltip("show");
-
-    // Tags Input
-    $(".tagsinput").tagsInput();
-
-    // jQuery UI Sliders
-    var $slider = $("#slider");
-    if ($slider.length) {
-      $slider.slider({
-        min: 1,
-        max: 5,
-        value: 2,
-        orientation: "horizontal",
-        range: "min"
-      }).addSliderSegments($slider.slider("option").max);
-    }
-
-    // Placeholders for input/textarea
-    $("input, textarea").placeholder();
-
-    // Make pagination demo work
-    $(".pagination a").on('click', function() {
-      $(this).parent().siblings("li").removeClass("active").end().addClass("active");
-    });
-
-    $(".btn-group a").on('click', function() {
-      $(this).siblings().removeClass("active").end().addClass("active");
-    });
-
-    // Disable link clicks to prevent page scrolling
-    $('a[href="#fakelink"]').on('click', function (e) {
-      e.preventDefault();
-    });
-
-    // Switch
-    $("[data-toggle='switch']").wrap('<div class="switch" />').parent().bootstrapSwitch();
-    
   });
-  
+
 })(jQuery);
