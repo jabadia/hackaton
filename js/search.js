0f3705ac305ea7dcc7e1b13eb98100ed8f21c106
francesc puigvert <francesc.puigvert@gmail.com>
2013-05-30 14:25:25 +0200

    buscar y localizar

--- js/search.js
+++ js/search.js
@@ -1,70 +1,116 @@
+dojo.require("esri.map");
+dojo.require("esri.layers.FeatureLayer");
+dojo.require("esri.tasks.query")
+dojo.require("esri.tasks.geometry");
+dojo.require("esri.layers.FeatureLayer");
+dojo.require("dojo.parser");
 
-function orientationChanged() {
-        if(map){
-          map.reposition();
-          map.resize();
-        }
+var map;
+var graphic;
+var currLocation;
+var watchId;
+var ptAct;
+
+
+$("#localizar").click(function() {
+	localizacionActual();
+});
+
+$("#buscar").click(function() {
+	bufferizar();
+});
+
+function init() {
+	map = new esri.Map("map",{
+		basemap:"topo",
+		center:[-71.121865, 42.370011],
+		zoom:13,
+		sliderStyle:"small"
+	});
+
 }
 
-function initFunc(map) {
-       dojo.connect(window, 'resize', map,map.resize);
-        if(navigator.geolocation){  
-          navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
-          watchId = navigator.geolocation.watchPosition(showLocation, locationError);
-        }
-        else{
-          alert("Browser doesn't support Geolocation. Visit http://caniuse.com to discover browser support for the Geolocation API.");
-        }
-      }
 
-function locationError(error) {
-        //error occurred so stop watchPosition
-        if(navigator.geolocation){
-          navigator.geolocation.clearWatch(watchId);
-        }
-        switch (error.code) {
-        case error.PERMISSION_DENIED:
-          alert("Location not provided");
-          break;
+function localizacionActual() {
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
+	} else {
+		alert("Este navegado no soporta la Geolocation.");
+	}
+}
 
-        case error.POSITION_UNAVAILABLE:
-          alert("Current location not available");
-          break;
+ function locationError(error) {
+	switch (error.code) {
+		case error.PERMISSION_DENIED:
+			alert("Sin permisos de localización");
+			break;
+		
+		case error.POSITION_UNAVAILABLE:
+			alert("Localización no disponible");
+			break;
 
-        case error.TIMEOUT:
-          alert("Timeout");
-          break;
+		case error.TIMEOUT:
+			alert("Finalizado el tiempo de espera");
+			break;
 
-        default:
-          alert("unknown error");
-          break;
-        }
-      }
+		default:
+			alert("Error desconocido");
+			break;
+	}
+}
 
 function zoomToLocation(location) {
-        var pt = esri.geometry.geographicToWebMercator(new esri.geometry.Point(location.coords.longitude, location.coords.latitude));
-        addGraphic(pt);    
-        map.centerAndZoom(pt, 12);
-      }
-
-function showLocation(location) {
-        //zoom to the users location and add a graphic
-        var pt = esri.geometry.geographicToWebMercator(new esri.geometry.Point(location.coords.longitude, location.coords.latitude));
-        if (!graphic) {
-          addGraphic(pt);
-        }
-        else { //move the graphic if it already exists
-          graphic.setGeometry(pt);
-        }
-        map.centerAt(pt);
+	var pt = esri.geometry.geographicToWebMercator(new esri.geometry.Point(location.coords.longitude, location.coords.latitude));
+	addGraphic(pt);
+	map.centerAndZoom(pt, 12);
+	ptAct=pt;
 }
-      
+
+
 function addGraphic(pt){
 	var symbol = new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 12, 
-          new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID,
-          new dojo.Color([210, 105, 30, 0.5]), 8), 
-          new dojo.Color([210, 105, 30, 0.9])
-        );
-        graphic = new esri.Graphic(pt, symbol);
-        map.graphics.add(graphic);
+	new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID,
+		new dojo.Color([210, 105, 30, 0.5]), 8), 
+		new dojo.Color([210, 105, 30, 0.9])
+	);
+	graphic = new esri.Graphic(pt, symbol);
+	map.graphics.add(graphic);
+
 }
+
+function bufferizar(){
+	//define input buffer parameters
+	if (ptAct== "undefined") {
+		localizacionActual();
+	} else {
+
+	var geometryService = new esri.tasks.GeometryService("http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
+		var params = new esri.tasks.BufferParameters();
+		params.geometries = [ ptAct ];
+
+		//buffer in linear units such as meters, km, miles etc.
+		params.distances = [ 5];
+		params.unit = esri.tasks.GeometryService.UNIT_KILOMETER;
+		params.outSpatialReference = map.spatialReference;
+		geometryService.buffer(params, showBuffer);
+	}
+}
+
+function showBuffer(geometries) {
+	var symbol = new esri.symbol.SimpleFillSymbol(
+		esri.symbol.SimpleFillSymbol.STYLE_SOLID,
+		new esri.symbol.SimpleLineSymbol(
+		esri.symbol.SimpleLineSymbol.STYLE_SOLID,
+		new dojo.Color([0,0,255,0.65]), 2
+		),
+		new dojo.Color([0,0,255,0.35])
+	);
+
+	dojo.forEach(geometries, function(geometry) {
+		var graphic = new esri.Graphic(geometry,symbol);
+		map.graphics.add(graphic);
+	});
+}		
+
+
+dojo.ready(init);
