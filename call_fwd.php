8ec70a2088ddf2b79d481754a5f9e5bdf4a9e13e
Javier Abadia <javi.abadia@gmail.com>
2013-06-02 19:37:58 +0200

    responde con el nombre real

--- call_fwd.php
+++ call_fwd.php
@@ -1,23 +1,64 @@
 <?php
+
+    /*
+     *   functions
+     */
+    function get_param($name,$default=null)
+    {
+        if( isset($_GET[$name]))
+            return $_GET[$name];
+        else if( isset($_POST[$name]))
+            return $_POST[$name];
+        else
+            return $default;
+    }
+
+    function query_by_phone($phone)
+    {
+        $service = 'http://services1.arcgis.com/w5PNyOikLERl9lIp/arcgis/rest/services/LoveHere_Features/FeatureServer/0';
+        $query = '/query?where='. urlencode("TELEFONO='".$phone."'");
+        $query .= '&outFields=Nick&returnGeometry=false&f=json';
+        $url = $service . $query;
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        $data = json_decode($data);
+        if( $data && count($data->features) > 0 )
+            return $data->features[0];
+        else
+            return null;
+    }
+
+    /*
+     *   request processing begins here
+     */
+
+    $from = get_param('From');
+    $from = str_replace('+34', '', $from);
+
+
     header("content-type: text/xml");
-// +34518880963
 
-	$objectid= isset($_GET['objectid'])? $_GET['objectid'] : 0;
-	$service = 'http://services1.arcgis.com/w5PNyOikLERl9lIp/arcgis/rest/services/LoveHere_Features/FeatureServer/0';
-	$query = '/query?where=&objectIds='.$objectid.'&outFields=TELEFONO&returnGeometry=false&f=json';
-	$url = $service . $query;
-    $ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $data = curl_exec($ch);
-    curl_close($ch);
-
-    $data = json_decode($data);
-    $number = $data->features[0]->attributes->TELEFONO;
-
-    $number = "656466110";
-
+    $user = query_by_phone($from);
+    if( $user )
+    {
+        $from_nick = $user->attributes->Nick;
+        $to_number = "656466110"; // hardcoded to avoid demo problems
 ?>
 <Response>
-    <Say voice="woman" language="es">Bienvenidos a Lofjiar punto com. Tu llamada sera transferida a la pareja seleccionada sin revelar tu numero</Say>
-  <Dial callerId="+34518880946">+34<?php print $number ?></Dial>
+    <Say voice="woman" language="es">Hola <?php print $from_nick ?>. Bienvenido a Lofjiar punto com. Tu llamada sera transferida a la pareja seleccionada sin revelar tu numero</Say>
+  <Dial callerId="+34518880946">+34<?php print $to_number ?></Dial>
 </Response>
+<?php
+    }
+    else
+    {
+?>
+<Response>
+    <Say voice="woman" language="es">Bienvenido a Lofjiar punto com. Lo sentimos pero el teléfono desde el que llama no pertenece a ningun usuario registrado.</Say>
+</Response>
+<?php
+    }
+?>
